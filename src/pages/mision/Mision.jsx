281bd3ed17aed8839d23fb9a6dcd0c8889ccded9
deyvigo <deyvi.gomez@unmsm.edu.pe>
2024-07-02 02:00:37 -0500

    final

--- src/pages/mision/Mision.jsx
+++ src/pages/mision/Mision.jsx
@@ -3,8 +3,8 @@ import wineImg from './../../assets/poomwine.jpeg'
 export const Mision = () => {
   return (
     <main className="text-white flex items-center flex-col p-4 gap-4">
-      <picture className="w-[300px] h-[320px] overflow-hidden rounded-xl shadow-lg border-2 border-red-600 hover:scale-105 transition-all duration-200 ease-in-out">
-        <img src={wineImg} className="w-[300px] h-[320px] object-cover" />
+      <picture className="w-[300px] h-[320px] overflow-hidden rounded-xl shadow-lghover:scale-105 transition-all duration-200 ease-in-out">
+        <img src={wineImg} className="w-[300px] h-[320px] object-cover border-2 border-red-600 fade-in" />
       </picture>
       <p className="w-[300px]">
         Brindar productos de calidad a nuestros clientes. Con este propósito deseñamos, desarrollamos y producimos nuestro propio vino de granada, cumpliendo con los más exigentes estándares de calidad.
--- src/pages/vision/Vision.jsx
+++ src/pages/vision/Vision.jsx
@@ -1,10 +1,10 @@
-import imgWine from './../../assets/wine.jpeg'
+import imgWine from './../../assets/poomer.jpeg'
 
 export const Vision = () => {
   return (
     <main className="text-white flex items-center flex-col p-4 gap-4">
-      <picture className="w-[300px] overflow-hidden rounded-xl shadow-lg border-2 border-red-600 hover:scale-105 transition-all duration-200 ease-in-out">
-        <img src={imgWine} alt="" />
+      <picture className="w-[300px] h-[320px] overflow-hidden rounded-xl shadow-lg hover:scale-105 transition-all duration-200 ease-in-out">
+        <img src={imgWine} className="object-cover w-[300px] h-[320px] border-2 border-red-600 rounded-xl fade-in" />
       </picture>
       <p className="w-[300px]">
         Para 2030, posicionar a nuestra marca como una de las mejores empresas de producción y vino de granada mercado Estadounidense con la ayuda de nuestro personal capacitado y comprometido con la organización y la sociedad para atender eficazmente las exigencias de nuestros clientes y accionistas
